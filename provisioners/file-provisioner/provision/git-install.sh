101c6fd85922f38ba6ee5682703ac52b92584a0f
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-10-14 02:26:51 +0200

    add resource clean-up to git-install script

--- provisioners/file-provisioner/provision/git-install.sh
+++ provisioners/file-provisioner/provision/git-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y git
+sudo apt-get update
+sudo apt-get install -y git
+
+# clean-up resources to reduce image size
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
